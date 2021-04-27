cc65d5aae1a75decbf4f8c2f291fc74d1a982a34
ancobacr <ancobacr@gmail.com>
2021-04-27 02:18:53 +0000

    testeo integration 2

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,12 +2,14 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import {MainNav} from "../component/main-nav"
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="text-center mt-5">
+            
 			<h1>Hello Rigo!</h1>
 			<img src={rigoImageUrl} />
 			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
@@ -17,6 +19,7 @@ export const Home = () => {
 					Read documentation
 				</a>
 			</p>
+            <MainNav />
 		</div>
 	);
 };
