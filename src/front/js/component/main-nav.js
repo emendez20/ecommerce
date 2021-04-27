02ad67b4292536e37a6ccadf70fc568e212547df
ancobacr <ancobacr@gmail.com>
2021-04-27 02:32:05 +0000

    correccion testeo

--- src/front/js/component/main-nav.js
+++ src/front/js/component/main-nav.js
@@ -6,49 +6,30 @@ import React from "react";
 
 export function MainNav() {
 	return (
-			<ul className="navbar-nav m-auto nav-flex-icons navbar-dark bg-dark fixed-bottom">
-				<li className="nav-item">
-					<Link to="/">
-						<a
-							className="nav-link waves-effect waves-light"
-							id="home"
-							href="#">
-							<i className="fa fa-fw fa-home"></i>
-						</a>
-					</Link>
-				</li>
+		<ul className="navbar-nav m-auto nav-flex-icons navbar-dark bg-dark fixed-bottom">
+			<li className="nav-item">
+				<a className="nav-link waves-effect waves-light" id="home" href="#">
+					<i className="fa fa-fw fa-home" />
+				</a>
+			</li>
 
-				<li className="nav-item">
-					<Link to="/user">
-						<a
-							className="nav-link waves-effect waves-light"
-							id="user"
-							href="#">
-							<i className="fas fa-user"></i>
-							<span className="badge badge-pill badge-light">
-								2
-							</span>
-						</a>
-					</Link>
-				</li>
+			<li className="nav-item">
+				<a className="nav-link waves-effect waves-light" id="user" href="#">
+					<i className="fas fa-user" />
+					<span className="badge badge-pill badge-light">2</span>
+				</a>
+			</li>
 
-				<li className="nav-item">
-					<Link to="/sales">
-						<a
-							className="nav-link waves-effect waves-light"
-							href="#">
-							<i className="fas fa-receipt"></i>
-							<span className="badge badge-pill badge-light">
-								3
-							</span>
-						</a>
-					</Link>
-				</li>
+			<li className="nav-item">
+				<a className="nav-link waves-effect waves-light" href="#">
+					<i className="fas fa-receipt" />
+					<span className="badge badge-pill badge-light">3</span>
+				</a>
+			</li>
 
-				<li className="nav-item">
-					<div>Button</div>
-				</li>
-			</ul>
-
+			<li className="nav-item">
+				<div>Button</div>
+			</li>
+		</ul>
 	);
 }
