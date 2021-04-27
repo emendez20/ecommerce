053e454f569d51e08fde4e9dda638fdba0261bff
ancobacr <ancobacr@gmail.com>
2021-04-27 02:23:20 +0000

    testeo 3

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -10,7 +10,7 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
             
-			<h1>Hello Rigo!</h1>
+			<h1>Hello!</h1>
 			<img src={rigoImageUrl} />
 			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
 			<p>
